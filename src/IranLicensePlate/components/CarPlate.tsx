f17e60dcdc76d634f68e8fa77a441b744bf21240
13951443 <mehran.khorrami@gmail.com>
2024-09-01 16:50:42 +0330

    init project

--- src/IranLicensePlate/components/CarPlate.tsx
+++ src/IranLicensePlate/components/CarPlate.tsx
@@ -16,13 +16,16 @@ export const CarPlate = (
 ) => {
     return (
         <div className={"car-plate-wrapper" + className} style={style}>
-            <div className="flag-wrapper">
+            <div className="plate-flag-wrapper">
                 <img src={flag} alt="flag"/>
                 <div className="iran-text">
                     <span>I.R.</span>
                     <span>IRAN</span>
                 </div>
             </div>
+            <div className="plate-main-section"></div>
+            <div className="plate-separator"></div>
+            <div className="plate-region-section"></div>
         </div>
     );
 }
