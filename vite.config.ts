04b4733e99522518863bc6778b8c475b4efedb3c
mehrankhorrami <mehran.uix@gmail.com>
2024-09-04 13:20:43 +0330

    fix build problem

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'), // Entry file for your library
+      name: 'MyReactLib', // Global variable name in UMD builds
+      fileName: (format) => `index.${format}.js` // Output file naming
+    },
+    rollupOptions: {
+      // Ensure external dependencies are not bundled into the library
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
 })
